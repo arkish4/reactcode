59c59cf491eaacfb56e293914e528a4a5c9d0e1e
Akshay Kalambe <a.kalambe@interproductec.com>
2023-05-05 10:38:23 +0530

    added basic code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 function App() {
+  const btnClick = () => {
+    // axios.post('http://localhost:3030')
+    // axios
+    //   .post('http://localhost:3030', { withCredentials: true })
+    //   .then((response) => console.log(response))
+    //   .catch((error) => console.log(error));
+    axios.post(
+      'http://localhost:3030',
+      { email: 'password' },
+      { withCredentials: true }
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={btnClick}>ClickMe</button>
     </div>
   );
 }
